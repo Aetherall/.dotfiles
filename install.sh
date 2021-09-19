87c85803f9230d20a97c0e7f392217be745f7e29
Aetherall <elieslou.dev@gmail.com>
2021-09-19 15:45:31 +0200

    feat(cloud): added docker gcloud and kube

--- install.sh
+++ install.sh
@@ -70,3 +70,21 @@ gsettings set org.gnome.desktop.interface gtk-theme "Ayu-Dark"
 
 echo "installing zinit"
 git clone https://github.com/zdharma/zinit.git ~/.sources/zinit
+
+echo "installing docker"
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+
+echo "installing gcloud & kubectl"
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt update && sudo apt install google-cloud-sdk kubectl -y
+gcloud init
+
+echo "installing k9s"
+curl -sS https://webinstall.dev/k9s | bash
