dd3578698bab5c42772c999d85d0692f070948e2
Aetherall <elieslou.dev@gmail.com>
2021-09-19 15:47:12 +0200

    feat(cloud): added docker-compose

--- install.sh
+++ install.sh
@@ -88,3 +88,7 @@ gcloud init
 
 echo "installing k9s"
 curl -sS https://webinstall.dev/k9s | bash
+
+echo "installing docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
